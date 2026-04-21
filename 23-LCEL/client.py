be72041550716c620e0d0f6a6c07c6abc188c62f
Ganesh Patil <ganeshkumar.patil@gmail.com>
2026-04-21 20:45:41 +0100

    update

--- 23-LCEL/client.py
+++ 23-LCEL/client.py
@@ -1,26 +1,23 @@
-import requests
 import streamlit as st
+from langserve import RemoteRunnable
 
 
-def get_groq_response(input_text):
-    json_body={
-  "input": {
-    "language": "French",
-    "text": "Hi"
-  },
-  "config": {},
-  "kwargs": {}
-}
-    response=requests.post("http://127.0.0.1:8000/chain/invoke",json_body)
+def get_groq_response(input_text, language):
+    chain = RemoteRunnable("http://127.0.0.1:8000/chain")
+    response = chain.invoke({"language": language, "text": input_text})
+    print(response)
+    return response
 
-    print(response.json())
-
-
-    return response.json()
 
 ## Streamlit app
 st.title("LLM Application Using LCEL")
-input_text=st.text_input("Enter the text you want to convert to french")
+
+language = st.selectbox(
+    "Select target language",
+    ["French", "Spanish", "German", "Italian", "Portuguese", "Hindi", "Japanese", "Chinese", "Arabic", "Russian"]
+)
+
+input_text = st.text_input("Enter the text you want to translate")
 
 if input_text:
-    st.write(get_groq_response(input_text))
+    st.write(get_groq_response(input_text, language))
